39087e94932c336f2c8539660000c60ce6904c0a
r.ghafouri <reza99gh@gmail.com>
2023-08-28 23:49:43 +0330

    all redux files which needed, added

---
+++ src/redux/cake/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+      break;
+    default:
+      return state;
+      break;
+  }
+};
+
+export default reducer;
