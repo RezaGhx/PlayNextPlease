17c43eac467062a41ac083105ed27dabd49c5034
r.ghafouri <reza99gh@gmail.com>
2023-08-31 16:37:12 +0330

    tailwind configs changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,33 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif'],
+    // },
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'purple': '#7e5bef',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'yellow': '#ffc82c',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+  }
+  },
+  variants: {
+    opacity: ({ after }) => after(['disabled']),
   },
   plugins: [],
-}
-export default config
+  // darkMode: ['class', '[data-mode="dark"]'],
+};
