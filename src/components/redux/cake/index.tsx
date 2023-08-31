0258099f0f50367370abf987e6fe1c1e920aff3a
r.ghafouri <reza99gh@gmail.com>
2023-08-31 16:37:38 +0330

    new action for cakes and payload has been used

--- src/components/redux/cake/index.tsx
+++ src/components/redux/cake/index.tsx
@@ -1,19 +1,30 @@
 // all commented parts all for the way we didn't use the react-redux built-in hooks
-
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 // import { connect } from 'react-redux';
-import { buyCake } from 'redux/cake/actions';
+import { buyCake, buyCakeByCount } from 'redux/cake/actions';
 
 // const Cake = (props) => {
 const Cake = () => {
+  const [count, setCount] = useState(0);
   const cakes = useSelector((state) => state.cake.numOfCakes);
   const dispatch = useDispatch();
   return (
-    <div>
-      {/* <h2>remaining cakes: {props.numOfCakes}</h2> */}
-      <h2>remaining cakes: {cakes}</h2>
+    <>
+      <div>
+        {/* <h2>remaining cakes: {props.numOfCakes}</h2> */}
+        <h2>remaining cakes: {cakes}</h2>
+      </div>
+      <div>
       <button onClick={() => dispatch(buyCake())}>buy cake</button>
-    </div>
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setCount(e.target.value)} />
+        <button onClick={() => dispatch(buyCakeByCount(count))}>
+          buy {count} cake
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/redux/cake/actions.ts
+++ src/redux/cake/actions.ts
@@ -1,7 +1,14 @@
-import { BUY_CAKE } from './types';
+import { BUY_CAKE, BUY_CAKE_BY_COUNT } from './types';
 
 export function buyCake() {
   return {
     type: BUY_CAKE,
+  };
+}
+
+export function buyCakeByCount(count = 1) {
+  return {
+    type: BUY_CAKE_BY_COUNT,
+    payload: count,
   };
 }
